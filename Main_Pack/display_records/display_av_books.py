3b30de2b3289fb3d12fc9d1e80a7f19b80f6207f
ankit-genzeon <ankitkumar.singh@genzeon.com>
2023-06-12 16:08:50 +0530

    "display all books"

--- Main_Pack/display_records/display_av_books.py
+++ Main_Pack/display_records/display_av_books.py
@@ -2,9 +2,31 @@ import sqlite3
 
 # Establish a connection to the database
 conn = sqlite3.connect('library_management.db')
+cursor = conn.cursor()
 
 
-def display_all_available_books():
-    records = conn.execute("select * from books")
-    for i in records:
-        print(i)
+def display_av_books():
+    # Query to retrieve available books from the "books" table
+    select_available_books_query = '''
+    SELECT * FROM books
+    WHERE available = 1;
+    '''
+    cursor.execute(select_available_books_query)
+    available_books = cursor.fetchall()
+
+    # Display the available books
+    print("Available books in the library:")
+    for book in available_books:
+        print("Book ID:", book[0])
+        print("Title:", book[1])
+        print("Author:", book[2])
+        print("Genre:", book[3])
+        print("Add Date:", book[4])
+        print("----------------------------")
+
+
+# Call the function to display available books
+display_av_books()
+
+# Close the connection
+conn.close()
--- Main_Pack/display_records/display_books.py
+++ Main_Pack/display_records/display_books.py
@@ -2,9 +2,17 @@ import sqlite3
 
 # Establish a connection to the database
 conn = sqlite3.connect('library_management.db')
-
+cursor = conn.cursor()
 
 def display_all_books():
-    records = conn.execute("select * from books")
-    for i in records:
-        print(i)
+    records = cursor.execute("select * from books")
+    all_books = cursor.fetchall()
+
+    print("All the books in the library:")
+    for book in all_books:
+        print("Book ID:", book[0])
+        print("Title:", book[1])
+        print("Author:", book[2])
+        print("Genre:", book[3])
+        print("Add Date:", book[4])
+        print("----------------------------")
