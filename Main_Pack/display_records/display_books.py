8a84a725cbd311572e5c0a02d8d7326ea1023aea
ankit-genzeon <ankitkumar.singh@genzeon.com>
2023-06-12 16:30:05 +0530

    merge

--- Main_Pack/display_records/display_books.py
+++ Main_Pack/display_records/display_books.py
@@ -4,7 +4,22 @@ import sqlite3
 conn = sqlite3.connect('library_management.db')
 
 
+
 def display_all_books():
+<<<<<<< Updated upstream
     records = conn.execute("select * from books")
     for i in records:
         print(i)
+=======
+    cursor.execute("select * from books")
+    all_books = cursor.fetchall()
+
+    print("All the books in the library:")
+    for book in all_books:
+        print("Book ID:", book[0])
+        print("Title:", book[1])
+        print("Author:", book[2])
+        print("Genre:", book[3])
+        print("Add Date:", book[4])
+        print("----------------------------")
+>>>>>>> Stashed changes
